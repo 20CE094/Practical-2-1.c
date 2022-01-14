aa52786e41832db46e1cff5b31d8289237a0f8f7
20CE094 <97758311+20CE094@users.noreply.github.com>
2022-01-15 00:30:51 +0530

    Add files via upload

---
+++ pratical2_1_c.py
@@ -0,0 +1,9 @@
+#Name-Nehal Patel
+#ID- 20CE094
+#Github link: https://github.com/20CE094/Practical-2-1.c.git
+#Aim: Write a Python program to sum all the items in a dictionary.
+a_dict = {1:1, 2:2, 3:3, 4:7}
+print(a_dict)
+values = a_dict.values()
+total = sum(values)
+print(total)
